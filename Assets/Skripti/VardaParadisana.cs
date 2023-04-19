edfe5ecc70b7d7484c9a3413ee1c140ae06d79c9
Bumerts <bumertstomijs@gmail.com>
2023-04-19 12:21:30 +0300

    Sagatavoties viss pirma skripta rakstisanai

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour 
+{
+	public int kadruSkaits = 0; 
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log("Uzsākta programma! Tu esi"+kadruSkaits+" kadrā!");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log("Kadrs: "+kadruSkaits);
+		kadruSkaits++;
+	}
+}
